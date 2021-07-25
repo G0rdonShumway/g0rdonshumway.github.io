9d87334914999efcae6cd209b1a5cb10f4600d70
G0rdonShumway <64680974+G0rdonShumway@users.noreply.github.com>
2021-07-26 00:36:00 +0400

    roulette fixes

--- roulette-pictures/js/main.js
+++ roulette-pictures/js/main.js
@@ -136,13 +136,13 @@ function fetchRouletteData() {
             rouletteResult.appendChild(newTableRow);
 
             newPicture.querySelectorAll('.red').forEach(cell => {
-                cell.parentElement.style.backgroundColor = 'hsl(0deg 100% 48% / 60%)';
+                cell.style.color = 'hsl(0deg 100% 48% / 60%)';
             })
             newPicture.querySelectorAll('.black').forEach(cell => {
-                cell.parentElement.style.backgroundColor = 'hsl(0deg 0% 0% / 60%)';
+                cell.style.color = 'hsl(0deg 0% 0% / 60%)';
             })
             newPicture.querySelectorAll('.green').forEach(cell => {
-                cell.parentElement.style.backgroundColor = 'hsl(120deg 100% 25% / 60%)';
+                cell.style.color = 'hsl(120deg 100% 25% / 60%)';
             })
 
         }
