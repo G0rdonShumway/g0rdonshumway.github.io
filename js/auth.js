8585a913c8e97d9ca426ff51148d924c21e8599a
G0rdonShumway <64680974+G0rdonShumway@users.noreply.github.com>
2022-12-28 01:03:01 +0400

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -4,7 +4,7 @@ const checkUser = () => {
   if (!username) {
     document.getElementById('dropdown_user').classList.remove('userIn')
     document.getElementById('userIcon').setAttribute('src', '../../img/user_out.png')
-    document.getElementById('profile').innerHTML = 'Log in to your account'
+    document.getElementById('profile').innerHTML = 'Log in to your account to save statistics'
   } else {
     document.getElementById('dropdown_user').classList.add('userIn')
     document.getElementById('userIcon').setAttribute('src', '../../img/user_in.png')
