0d749635f7504c3c32138e9e649b0cd1ce1a584d
G0rdonShumway <64680974+G0rdonShumway@users.noreply.github.com>
2022-12-29 03:36:06 +0400

    Update dbfunctions.js

--- js/dbfunctions.js
+++ js/dbfunctions.js
@@ -63,7 +63,7 @@ const saveData = (game, percentage, time) => {
             .get()
             .then((doc) => {
               if (doc.exists) {
-                console.log(+doc.data().time)
+                console.log(doc.data())
                 console.log(+time.slice(0, -4))
                 if (+doc.data().time < +time.slice(0, -4)) {
                   db.collection(game).doc(getUsername()).set({
