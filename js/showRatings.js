f9dc5292d87834f0eb57bb3bd29cc268426f3592
G0rdonShumway <64680974+G0rdonShumway@users.noreply.github.com>
2023-01-03 20:02:59 +0400

    test out

--- js/showRatings.js
+++ js/showRatings.js
@@ -11,7 +11,6 @@ const showRatings = (game) => {
         element.time = doc.data().time
 
         chart.push(element)
-
       });
       function compare(a, b) {
         if (a.time < b.time) {
@@ -40,6 +39,59 @@ const showRatings = (game) => {
           <div class="ratings-time">${item.time}</div>
         </div>`
       })
+
+      let isInChart = newChart.some(findInChart);
+
+      function findInChart(value) {
+        return value.name === localStorage.getItem('username');
+      }
+
+      if (!isInChart) {
+        db.collection('tests').doc(game).collection(localStorage.getItem('username'))
+          .get()
+          .then((querySnapshot) => {
+            var personalChart = []
+            querySnapshot.forEach((doc) => {
+              // doc.data() is never undefined for query doc snapshots
+              var element = {}
+              element.correctAnswers = doc.data().correctAnswers
+              element.time = doc.data().time
+
+              personalChart.push(element)
+
+            });
+
+
+            function comparePersonalBest(a, b) {
+              if (a.correctAnswers > b.correctAnswers) {
+                return -1;
+              }
+              if (a.correctAnswers < b.correctAnswers) {
+                return 1;
+              }
+              return 0;
+            }
+            personalBest = personalChart.sort(comparePersonalBest)
+
+            var newRatingsItem = document.createElement('div')
+            newRatingsItem.classList.add("ratings-item")
+            document.getElementById('ratings').appendChild(newRatingsItem)
+
+            newRatingsItem.outerHTML = `
+              <div class="ratings-item ratings-personal">
+                <div class="ratings-name">
+                  Your personal best is ${personalBest[0].correctAnswers}% in ${personalBest[0].time}
+                </div >
+                
+              </div > `
+          })
+          .catch((error) => {
+            console.log("Error getting documents: ", error);
+          });
+
+
+
+      }
     })
     .catch((error) => {
       console.log("Error getting documents: ", error);
