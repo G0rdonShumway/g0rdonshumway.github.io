65e7ca1b85a1c76278978c707992122d4b66a4c3
G0rdonShumway <64680974+G0rdonShumway@users.noreply.github.com>
2023-01-25 00:25:02 +0400

    Update dau.js

--- js/dau.js
+++ js/dau.js
@@ -1,9 +1,12 @@
 const getDAU = async () => {
+    console.log('in')
     try {
         // Get all documents in the 'dailyUsage' collection
         const snapshot = await db.collection('dailyUsage').get();
+        console.log('snapshot')
         const data = await Promise.all(snapshot.docs.map(async doc => {
             const collections = await doc.ref.listCollections();
+            console.log('collections')
             return {
                 date: doc.id,
                 numSubcollections: collections.size
@@ -11,6 +14,7 @@ const getDAU = async () => {
         }));
         data.forEach(({date, numSubcollections}) => {
             db.collection('DAU').doc(date).set({numSubcollections});
+            console.log('DAU')
         });
     } catch (err) {
         console.log('Error getting dailyUsage documents:', err);
