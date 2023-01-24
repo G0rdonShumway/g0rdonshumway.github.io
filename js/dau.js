82ef87f2b657bb6426897d3ed98dad4755c838ae
G0rdonShumway <64680974+G0rdonShumway@users.noreply.github.com>
2023-01-24 23:26:14 +0400

    Update dau.js

--- js/dau.js
+++ js/dau.js
@@ -1,17 +1,25 @@
-// Get all documents in the 'daily' collection
+// Get all documents in the 'dailyUsage' collection
 db.collection('dailyUsage').get()
   .then(snapshot => {
     snapshot.forEach(doc => {
       // Get date from document ID
       const date = doc.id;
-      // Use the `size` method to get the number of documents
-      const numUsers = snapshot.size;
-      // Create a new 'DAU' collection if it does not exist
-      db.collection('DAU').doc(date).set({
-        numUsers: numUsers
-      });
+      
+      // Get subcollections
+      doc.ref.listCollections()
+        .then(collections => {
+            // Use the `size` method to get the number of subcollections
+            const numSubcollections = collections.size;
+            // Create a new 'DAU' collection if it does not exist
+            db.collection('DAU').doc(date).set({
+              DAU: numSubcollections
+            });
+        })
+        .catch(err => {
+          console.log('Error getting subcollections:', err);
+        });
     });
   })
   .catch(err => {
-    console.log('Error getting daily documents:', err);
+    console.log('Error getting dailyUsage documents:', err);
   });
