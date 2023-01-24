8774b23435d182a5a4be2965289463c9c95be434
G0rdonShumway <64680974+G0rdonShumway@users.noreply.github.com>
2023-01-24 23:16:32 +0400

    Update dau.js

--- js/dau.js
+++ js/dau.js
@@ -1,5 +1,5 @@
 // Get all documents in the 'daily' collection
-firestore.collection('dailyUsage').get()
+db.collection('dailyUsage').get()
   .then(snapshot => {
     snapshot.forEach(doc => {
       // Get date from document ID
@@ -7,7 +7,7 @@ firestore.collection('dailyUsage').get()
       // Use the `size` method to get the number of documents
       const numUsers = snapshot.size;
       // Create a new 'DAU' collection if it does not exist
-      firestore.collection('DAU').doc(date).set({
+      db.collection('DAU').doc(date).set({
         numUsers: numUsers
       });
     });
