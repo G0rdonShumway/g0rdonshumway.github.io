e25d6598b1ff04ba090c8e4f62eed48195c8cf30
G0rdonShumway <64680974+G0rdonShumway@users.noreply.github.com>
2023-01-25 00:01:55 +0400

    Update dau.js

--- js/dau.js
+++ js/dau.js
@@ -1,31 +1,21 @@
-// Get all documents in the 'dailyUsage' collection
-function updateDAU(dbRef){
-  dbRef.collection('dailyUsage').get()
-    .then(snapshot => {
-      snapshot.forEach(snap => {
-        // Get date from document ID
-        const date = snap.id;
-        console.log(date)
-        
-        // Get subcollections
-        snap.ref.listCollections()
-          .then(collections => {
-              // Use the `size` method to get the number of subcollections
-              const numSubcollections = collections.size;
-              console.log(numSubcollections)
-              // Create a new 'DAU' collection if it does not exist
-              dbRef.collection('DAU').doc(date).set({
-                DAU: numSubcollections
-              });
-          })
-          .catch(err => {
-            console.log('Error getting subcollections:', err);
-          });
-      });
-    })
-    .catch(err => {
-      console.log('Error getting dailyUsage documents:', err);
-    });
-  }
-updateDAU(db)
+const firestore = require('firebase-admin').firestore();
 
+const getDAU = async () => {
+    try {
+        // Get all documents in the 'dailyUsage' collection
+        const docs = await firestore.collection('dailyUsage').listDocuments();
+        const data = await Promise.all(docs.map(doc => {
+            return doc.listCollections().then(collections => {
+                return {
+                    date: doc.id,
+                    numSubcollections: collections.length
+                }
+            })
+        }));
+        data.forEach(({date, numSubcollections}) => {
+            firestore.collection('DAU').doc(date).set({numSubcollections});
+        });
+    } catch (err) {
+        console.log('Error getting dailyUsage documents:', err);
+    }
+}
